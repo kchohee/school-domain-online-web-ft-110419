fccf15e25affa8fb0ecb1c5369a021f33bcdb02d
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 20:33:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,28 @@
 class School
-  def initialized(name)
+  def initialize(name)
     @name = name
+    @roster = {}
   end
-end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
