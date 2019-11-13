f92ed782fe043e82955fe47afc0a0edcf1f7bda1
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 21:00:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,24 @@
 require 'pry'
+
+
 class School
   attr_accessor :roster
-
   def initialize(name)
     @name = name
     @roster = {}
   end
-
   def add_student(student, grade)
     if roster[grade] == nil
       roster[grade] = []
     end
     roster[grade] << student
   end
-
   def grade(grade)
     roster[grade]
   end
-
   def sort
     roster.each { |k, v| roster[k] = v.sort }
 binding.pry
   end
 end
+add_student("Cho", 28)
