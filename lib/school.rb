f5ed37cd6b63d22409b5e1290395d7781b9fa1bd
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 21:29:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
       @roster[grade]<< student_name
     else
       @roster[grade] = []
-      # @roster[grade] << student_name
+      @roster[grade] << student_name
     end
   end
   def grade(grade)
